bd362de652ba48ec7962dcb4a9e56ff8c7197c6f
quanghuong <quanghuong.dev@gmail.com>
2024-06-17 21:13:00 +0700

    commit 2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: 'http://localhost:6868',
+        origin: 'https://writing-assistant.vercel.app',
         credentials: true,
     })
 );
