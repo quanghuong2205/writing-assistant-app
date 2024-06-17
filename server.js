a3ba82bb1c0eb3969300cd1e99dab9fa9cac2028
quanghuong <quanghuong.dev@gmail.com>
2024-06-17 21:33:03 +0700

    commit3

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const server = require('./src/app');
 require('dotenv').config();
-// const DBConnector = require('./src/helper/db-connector.helper');
+const DBConnector = require('./src/helper/db-connector.helper');
 
 /* Error route */
 
 /* Open the server */
 server.listen(process.env.SERVER_PORT, () => {
-    console.log(
-        `Server is listening on :: http://${process.env.SERVER_DOMAIN}:${process.env.SERVER_PORT}`
-    );
+    console.log(`Server is listening`);
 });
 
 /* Close the server */
 process.on('SIGINT', async () => {
     /* Close the database connector */
-    // await DBConnector.close();
+    await DBConnector.close();
 
     /* Close the server */
-    console.log(
-        `Closed the server on :: http://${process.env.SERVER_DOMAIN}:${process.env.SERVER_PORT}`
-    );
+    console.log(`Closed the server`);
     process.exit();
 });
