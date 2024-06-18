1875261d78f6a62749eef174a5c725de5b3b2280
quanghuong <quanghuong.dev@gmail.com>
2024-06-18 16:38:08 +0700

    commit 37

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ app.use(cookieParser());
 app.use('/test', (req, res, next) => {
     res.cookie('myCookie', 'cookieValue', {
         secure: true,
+        domain: 'test-flax-six-94.vercel.app',
     });
 
-    res.redirect(`https://writing-assistant-nine.vercel.app/login`);
+    res.redirect(`https://test-flax-six-94.vercel.app`);
 });
 
 app.use('/', (req, res, next) => {
