c6805aa1f18f30eb5f3efdb9c3f79ab919eb29d4
quanghuong <quanghuong.dev@gmail.com>
2024-06-18 15:48:55 +0700

    commit 23

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ app.use('/', (req, res, next) => {
     res.send('Server is running now');
 });
 
-/* Init routers */
-const initRoutes = require('./src/routes');
-initRoutes(app);
-
 /* Catch error */
 app.use((error, req, res, next) => {
     res.status(error?.status || 500).json({
