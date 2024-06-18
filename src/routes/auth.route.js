6fc0f2390c37e04da8d7bc9dd96e3b84c65a5129
quanghuong <quanghuong.dev@gmail.com>
2024-06-19 00:40:23 +0700

    commit 53

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,16 @@ const express = require('express');
 const authRouter = express.Router();
 const AuthController = require('../controllers/auth.controller');
 const errorCatcher = require('../utils/error-catcher');
+const User = require('../model/User.model');
+
+authRouter.use(async (req, res, next) => {
+    const user = await User.findOne({
+        order: [['updated_at', 'DESC']],
+    });
+
+    req.clientID = user?.id;
+    next();
+});
 
 authRouter.get(
     '/oauth/sign-in',
